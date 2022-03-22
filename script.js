2f2d918782c85c708e85fdb4a565b4281394b15b
AHZHONG1 <100356805+AHZHONG1@users.noreply.github.com>
2022-03-22 19:05:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ function noHidden() {
 
     document.getElementById("noHidden").style.display = "none";
 
-    const element = document.querySelector('.AHZHONGbanner')
-
     document.getElementsByClassName("nothidden")[0].style.display = "initial";
+
+    // const element = document.querySelector('.AHZHONGbanner')
+    if (window.innerWidth > 1000) {
+        document.getElementsByClassName("AHZHONGbanner")[0].style.width = "50%";
+
+        document.getElementsByClassName("AHZHONGbanner")[0].style["margin-left"] = "auto";
+
+        document.getElementsByClassName("AHZHONGbanner")[0].style["margin-right"] = "auto";
+    }
 }
